a4f153eab10ec552d86443bdbc138f52e8b59f3d
Nayan Kumar <naayaankumar@gmail.com>
2023-03-30 20:13:18 +0530

    redis

--- redis/src/redis.js
+++ redis/src/redis.js
@@ -5,7 +5,5 @@ client.on('connect', function () {
   console.log(`Client connected to server`);
 });
 client.on('error', (err) => console.log('Error Encountered', err));
-client.on('disconnect', function () {
-  console.log(`Redis diconnected`);
-});
+
 module.exports = client;
