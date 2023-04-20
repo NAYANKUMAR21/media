10bb5d0c82a8e6abb3c1a03211eab250d0348978
Nayan Kumar <naayaankumar@gmail.com>
2023-04-20 21:46:57 +0530

    ADDED reacy native app

--- todo_app/App.js
+++ todo_app/App.js
@@ -1,24 +1,101 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  const [state, setState] = useState([]);
-  const [sent, setStatement] = useState([]);
-  
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+import {
+  Button,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'skyblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 300,
+  },
+  Ai: {
+    padding: 10,
+    color: 'black',
+    fontSize: 20,
+    borderWidth: 1,
+    borderRadius: 10,
+  },
 });
+
+export default function App() {
+  const [state, setState] = useState([]);
+  const [sent, setStatement] = useState({
+    email: '',
+    password: false,
+  });
+
+  const handleEmail = (e) => {
+    // console.log(e);
+    setStatement({ ...sent, email: e });
+  };
+  const handlePassword = (e) => {
+    // console.log(e);
+    setStatement({ ...sent, password: e });
+  };
+  const handleClick = () => {
+    console.log(sent, state);
+    setState([...state, sent]);
+  };
+  const handleFilter = (id) => {
+    let x = state?.filter((el, index) => {
+      return id !== index;
+    });
+    setState(x);
+  };
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={{ fontSize: 50, fontWeight: 'bold' }}>Nayan Kumar</Text>
+        <SafeAreaView style={styles.input}>
+          <Text style={{ marginTop: 50 }}>User Name</Text>
+          <TextInput
+            value={sent.title}
+            style={styles.Ai}
+            onChangeText={handleEmail}
+          />
+          <Text style={{ marginTop: 50 }}>password</Text>
+          <TextInput
+            value={sent.title}
+            style={styles.Ai}
+            onChangeText={handlePassword}
+          />
+          <Pressable
+            style={{
+              borderWidth: 1,
+              width: 120,
+              borderRadius: 10,
+              padding: 10,
+              margin: 'auto',
+            }}
+            onPress={handleClick}
+          >
+            <Text>I'm pressable!</Text>
+          </Pressable>
+          <View>
+            {state?.map((el, index) => {
+              return (
+                <View key={index} style={{ flexDirection: 'row', gap: 10 }}>
+                  <Text>{el.email}</Text>
+                  <Text>{el.password}</Text>
+                  <Button title="delete" onPress={() => handleFilter(index)} />
+                </View>
+              );
+            })}
+          </View>
+        </SafeAreaView>
+      </View>
+    </>
+  );
+}
