9cc369798066e114d32e1ebb34a4b1a58e72afca
Nayan Kumar <naayaankumar@gmail.com>
2023-05-10 20:07:31 +0530

    uploaded redis cloud nodejs

---
+++ R_cloud/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const redis = require('redis');
+const jwt = require('jsonwebtoken');
+const client = redis.createClient({
+  host: 'redis://red-chdqnd2k728nnn2n11hg',
+  port: '6379',
+});
+client.on('error', () => {
+  console.log(`Redis client Error`, err);
+});
+client.on('connect', () => {
+  console.log('Redis COnnected');
+});
+app.get('/', async (req, res) => {
+  try {
+    await client.connect();
+    let y = await client.get('token');
+
+    if (y) {
+      return res.status(200).send({ message: y });
+    }
+    const token = jwt.sign(
+      {
+        id: '123',
+        name: 'Nayan Kumar',
+        success: '21 june 2024',
+      },
+      'REDIS'
+    );
+    await client.set('x', token);
+    let z = await client.get('x');
+    await client.disconnect();
+    return res.status(200).send({ message: z });
+  } catch (er) {
+    return res.status(404).send({ message: er.message });
+  }
+});
+app.listen(8080, () => {
+  console.log('http://localhost:8080');
+});
