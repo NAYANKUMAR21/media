352841c466c4e7bc6c8bc009c8b22c1238e97f25
Nayan Kumar <naayaankumar@gmail.com>
2023-05-16 16:15:43 +0530

    redis with next

---
+++ redis-next/Models/user.model.js
@@ -0,0 +1,65 @@
+[
+  {
+    restaurant_name: 'Tasty Treats',
+    description: 'A delicious selection of dishes',
+    location: '123 Main Street',
+    city: 'New York',
+    state: 'NY',
+    zip: '10001',
+    phone: '(555) 555-1212',
+    restaurant_sm_image:
+      'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426',
+    restaurant_md_image:
+      'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426',
+    restaurant_lg_image:
+      'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa',
+
+    menu: [
+      {
+        item_name: 'Pizza',
+        description:
+          'Freshly baked pizza with tomato sauce and mozzarella cheese',
+        price: 10.99,
+        item_sm_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426',
+        item_md_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426',
+        item_lg_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa',
+      },
+      {
+        item_name: 'Burger',
+        description: 'Juicy beef burger with lettuce, tomato, and pickles',
+        price: 8.99,
+        item_sm_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426',
+        item_md_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426',
+        item_lg_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa',
+      },
+      {
+        item_name: 'Pasta',
+        description: 'Al dente pasta with your choice of sauce',
+        price: 12.99,
+        item_sm_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426',
+        item_md_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426',
+        item_lg_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa',
+      },
+      {
+        item_name: 'Salad',
+        description: 'Fresh greens with a variety of toppings',
+        price: 6.99,
+        item_sm_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426',
+        item_md_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa2525426',
+        item_lg_image:
+          'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/56c9ab92bd79745fd152a30fa',
+      },
+    ],
+  },
+];
